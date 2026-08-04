2334ace3c35d2538dc47d79f4dce3db58ab31d8d
Fernando Oropeza <100188482+OropezaNtx@users.noreply.github.com>
2026-08-04 01:47:33 -0600

    Scroll to top on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { Routes, Route } from "react-router-dom"
+import { useEffect } from "react"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import ProjectDetail from "./pages/ProjectDetail"
 import NotFound from "./pages/NotFound"
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" })
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/project/:id" element={<ProjectDetail />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   )
 }
 
